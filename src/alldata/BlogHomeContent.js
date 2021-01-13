b43171cd0340ed7b998cc51bd56073248d5e4bac
himawannurcahyanto97 <63728710+himawannurcahyanto97@users.noreply.github.com>
2021-01-13 19:42:35 +0900

    Update BlogHomeContent.js

--- src/alldata/BlogHomeContent.js
+++ src/alldata/BlogHomeContent.js
@@ -2,5 +2,5 @@ import bloghima from "../images/bloghima.png";
 
 export const BlogHomeContent = {
     'img': bloghima,
-    'desc':'testestes'
+    'desc':'Great stories that I would like to share with you about my daily life and my journey. Scroll down to take a look. Enjoy!'
 }
