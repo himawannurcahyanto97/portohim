17f590f6d4caf2130feceaae4ef7d8b7c96c3dcf
himawannurcahyanto97 <63728710+himawannurcahyanto97@users.noreply.github.com>
2021-01-13 17:14:03 +0900

    Update BlogHomeContent.js

--- src/alldata/BlogHomeContent.js
+++ src/alldata/BlogHomeContent.js
@@ -2,5 +2,5 @@ import bloghima from "../images/bloghima.png";
 
 export const BlogHomeContent = {
     'img': bloghima,
-    'desc':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
+    'desc':'testestes'
 }
